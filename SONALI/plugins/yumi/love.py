d312fd68f41d1fdafa4a6c4af08275a1a5ab6fd1
Jani-Music <hahahsygs395@gmail.com>
2025-06-30 00:33:42 +0530

    Update love.py

--- SONALI/plugins/yumi/love.py
+++ SONALI/plugins/yumi/love.py
@@ -26,7 +26,7 @@ def get_random_message(love_percentage):
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/ZOYUMUSICBOT?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Jani_Music_Robot?startgroup=true"),
     ],
 ]
 
